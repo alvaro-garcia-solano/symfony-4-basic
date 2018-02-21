8eaaedb3d1a767d955f39fe33806fc5407ba0f36
alvaro_garcia <a.garciasolano@gmail.com>
2018-02-21 09:43:44 +0100

    Empieza la clase 13 de Services

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,11 +28,20 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-        //dump($slug, $this);
+        dump($slug, $this);
 
         return $this->render('article/show.html.twig',[
             'title' =>  ucwords(str_replace('-',' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        return new JsonResponse(['hearts' => rand(5,100)]);
     }
 }
